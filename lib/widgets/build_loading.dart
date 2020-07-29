b68b762f2275912aef614e43a8fbb30ffff19783
aloRanking <adigunlateef02@gmail.com>
2020-07-29 17:28:03 +0100

    refactor code

--- lib/widgets/build_loading.dart
+++ lib/widgets/build_loading.dart
@@ -4,18 +4,13 @@ class BuildLoadingWidget extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
      return Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: [
-            SizedBox(
-              height: 25.0,
-              width: 25.0,
-              child: CircularProgressIndicator(
-                valueColor: new AlwaysStoppedAnimation<Color>(Colors.white),
-                strokeWidth: 4.0,
-              ),
-            )
-          ],
+        child: SizedBox(
+          height: 25.0,
+          width: 25.0,
+          child: CircularProgressIndicator(
+            valueColor: new AlwaysStoppedAnimation<Color>(Colors.white),
+            strokeWidth: 4.0,
+          ),
         ));
   }
 }
